2f147e77ceb3ba91e4bcad9c17655f38768adcab
itsTalwar <nt@devrev.ai>
2023-11-18 15:09:08 +0530

    Adding raw duckdb runner

--- benchmarking/benchmarking-app/src/app/app.tsx
+++ benchmarking/benchmarking-app/src/app/app.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { MemoryDBMProvider } from './dbm-context/memory-dbm-context';
+import { RawDBMProvider } from './dbm-context/raw-dbm-context';
 import { FileLoader } from './file-loader/file-loader';
 import { QueryBenchmarking } from './query-benchmarking/query-benchmarking';
 
@@ -7,6 +8,18 @@ export function App() {
   return (
     <Router>
       <Routes>
+        <Route
+          path="/raw-dbm"
+          element={
+            <div>
+              <RawDBMProvider>
+                <FileLoader>
+                  <QueryBenchmarking />
+                </FileLoader>
+              </RawDBMProvider>
+            </div>
+          }
+        />
         <Route
           path="/memory-dbm"
           element={
