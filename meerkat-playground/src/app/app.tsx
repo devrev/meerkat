4a62235d663763f23f69133ab73960c468877389
itsTalwar <nt@devrev.ai>
2024-03-03 21:27:18 +0530

    Adding dark mode running

--- meerkat-playground/src/app/app.tsx
+++ meerkat-playground/src/app/app.tsx
@@ -3,6 +3,14 @@
 import { QueryBenchmarking } from '../components/query-benchmarking';
 import { Sidebar } from '../components/sidebar';
 import { IndexedDBMProvider } from '../duckdb/indexed-dbm-context';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
 
 export function App() {
   return (
@@ -14,6 +22,19 @@ export function App() {
           <IndexedDBMProvider>
             <div>
               <div className="p-2">Hello World Inside</div>
+              <Dialog>
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+
               <QueryBenchmarking />
             </div>
           </IndexedDBMProvider>
