cea6b5c21d47728545cc2706e14a55810d850590
Radhika <radhika.sigtia@devrev.ai>
2024-03-29 15:53:24 +0530

    cube to table schema

---
+++ meerkat-core/src/utils/cube-to-table-schema.spec.ts
@@ -0,0 +1,47 @@
+import { convertCubeToTableSchema } from './cube-to-table-schema';
+describe('cube-to-table-schema', () => {
+  it('Should convert to table schema', async () => {
+    const files = [
+      'cube(`users`, { sql_table: `public.users`, joins: {}, dimensions: { id: { sql: `id`, type: `number`, primary_key: true }, gender: { sql: `gender`, type: `string` }, city: { sql: `city`, type: `string` }, state: { sql: `state`, type: `string` }, first_name: { sql: `first_name`, type: `string` }, company: { sql: `company`, type: `string` }, last_name: { sql: `last_name`, type: `string` }, created_at: { sql: `created_at`, type: `time` } }, measures: { count: { type: `count` } }, pre_aggregations: {} });',
+      'cube(`line_items`, { sql_table: `public.line_items`, joins: { orders: { sql: `${CUBE}.order_id = ${orders}.id`, relationship: `many_to_one` }, products: { sql: `${CUBE}.product_id = ${products}.id`, relationship: `many_to_one` } }, dimensions: { id: { sql: `id`, type: `number`, primary_key: true }, created_at: { sql: `created_at`, type: `time` } }, measures: { count: { type: `count` }, price: { sql: `price`, type: `sum` }, quantity: { sql: `quantity`, type: `sum` } }, pre_aggregations: {} });',
+    ];
+    const outputTableSchemas = [
+      {
+        name: 'users',
+        sql: 'public.users',
+        measures: [{ name: 'count', sql: 'COUNT(*)', type: 'number' }],
+        dimensions: [
+          { name: 'id', sql: 'id', type: 'number' },
+          { name: 'gender', sql: 'gender', type: 'string' },
+          { name: 'city', sql: 'city', type: 'string' },
+          { name: 'state', sql: 'state', type: 'string' },
+          { name: 'first_name', sql: 'first_name', type: 'string' },
+          { name: 'company', sql: 'company', type: 'string' },
+          { name: 'last_name', sql: 'last_name', type: 'string' },
+          { name: 'created_at', sql: 'created_at', type: 'time' },
+        ],
+      },
+      {
+        name: 'line_items',
+        sql: 'public.line_items',
+        measures: [
+          { name: 'count', sql: 'COUNT(*)', type: 'number' },
+          { name: 'price', sql: 'SUM(price)', type: 'number' },
+          { name: 'quantity', sql: 'SUM(quantity)', type: 'number' },
+        ],
+        dimensions: [
+          { name: 'id', sql: 'id', type: 'number' },
+          { name: 'created_at', sql: 'created_at', type: 'time' },
+        ],
+        joins: [
+          { sql: 'MEERKAT.order_id = orders.id' },
+          { sql: 'MEERKAT.product_id = products.id' },
+        ],
+      },
+    ];
+    for (let i = 0; i < files.length; i++) {
+      const tableSchema = convertCubeToTableSchema(files[i]);
+      expect(tableSchema).toEqual(outputTableSchemas[i]);
+    }
+  });
+});
---
+++ meerkat-core/src/utils/cube-to-table-schema.ts
@@ -0,0 +1,81 @@
+import { TableSchema } from '@devrev/meerkat-core';
+
+export function convertCubeToTableSchema(file: string): TableSchema | null {
+  const cube = (name: any, object: any) => ({ name, object });
+  let replacedFile = file.replace(/\${(.*?)}/g, (match, variable) => variable);
+  replacedFile = replacedFile.replace(/CUBE/g, 'MEERKAT');
+  const { name, object } = eval(replacedFile);
+  const resObj: TableSchema = {
+    name,
+    sql: object.sql_table,
+    measures: [],
+    dimensions: [],
+  };
+  const dimensions = object.dimensions;
+  const measures = object.measures;
+  for (const key in dimensions) {
+    resObj.dimensions.push({
+      name: key,
+      sql: dimensions[key].sql,
+      type: dimensions[key].type,
+    });
+  }
+  for (const key in measures) {
+    resObj.measures.push({ name: key, ...convertMeasure(measures[key]) });
+  }
+  if (object.joins && Object.keys(object.joins).length > 0) {
+    resObj.joins = [];
+    for (const joinName in object.joins) {
+      const join = object.joins[joinName];
+      resObj.joins.push({
+        sql: join.sql,
+      });
+    }
+  }
+  return resObj;
+}
+
+function convertMeasure(measure: any): any {
+  switch (measure.type) {
+    case 'count':
+      return {
+        sql: measure.sql ? 'COUNT(' + measure.sql + ')' : 'COUNT(*)',
+        type: 'number',
+      };
+    case 'count_distinct':
+      return {
+        sql: 'COUNT(DISTINCT ' + measure.sql + ')',
+        type: 'number',
+      };
+    case 'count_distinct_approx':
+      return {
+        sql: 'COUNT(DISTINCT ' + measure.sql + ')',
+        type: 'number',
+      };
+    case 'sum':
+      return {
+        sql: 'SUM(' + measure.sql + ')',
+        type: 'number',
+      };
+    case 'avg':
+      return {
+        sql: 'AVG(' + measure.sql + ')',
+        type: 'number',
+      };
+    case 'min':
+      return {
+        sql: 'MIN(' + measure.sql + ')',
+        type: 'number',
+      };
+    case 'max':
+      return {
+        sql: 'MAX(' + measure.sql + ')',
+        type: 'number',
+      };
+    default:
+      return {
+        sql: measure.sql,
+        type: measure.type,
+      };
+  }
+}
