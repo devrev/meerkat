edf1947012fe7403116858a65a3933a6ab489caf
Zaid Jan <98650818+zaidjan-devrev@users.noreply.github.com>
2024-06-10 16:56:16 +0530

    Optimization:  group by only when measures has some length (#82)
    * group by only when measures has some lenght
    
    * joins test cases update
    
    * added test case
    
    * bumped package json

--- meerkat-node/src/__tests__/joins.spec.ts
+++ meerkat-node/src/__tests__/joins.spec.ts
@@ -385,7 +385,7 @@ describe('Joins Tests', () => {
     const parsedOutput = JSON.parse(JSON.stringify(output));
     console.info('parsedOutput', parsedOutput);
     expect(sql).toEqual(
-      'SELECT  orders__order_id FROM (SELECT *, orders.order_id AS orders__order_id FROM (select * from orders) AS orders) AS MEERKAT_GENERATED_TABLE GROUP BY orders__order_id'
+      'SELECT  orders__order_id FROM (SELECT *, orders.order_id AS orders__order_id FROM (select * from orders) AS orders) AS MEERKAT_GENERATED_TABLE'
     );
     expect(parsedOutput).toHaveLength(11);
   });
