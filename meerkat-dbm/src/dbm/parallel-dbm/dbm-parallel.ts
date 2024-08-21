b8839b4eb07d5717cc05cfe7b8852f6e173ba5d2
itsTalwar <nt@devrev.ai>
2024-08-21 14:23:19 +0530

    WIP

--- meerkat-dbm/src/dbm/parallel-dbm/dbm-parallel.ts
+++ meerkat-dbm/src/dbm/parallel-dbm/dbm-parallel.ts
@@ -1,15 +1,19 @@
 import { FileManagerType } from '../../file-manager/file-manager-type';
 import { DBMEvent, DBMLogger } from '../../logger';
+import {
+  BROWSER_RUNNER_TYPE,
+  BrowserRunnerExecQueryMessage,
+} from '../../window-communication/runner-types';
 import { DBMConstructorOptions, QueryOptions, TableConfig } from '../types';
-import { InstanceManagerType } from './../instance-manager';
+import { IFrameManager } from './iframe-manager';
 
 export class DBMParallel {
   private fileManager: FileManagerType;
-  private instanceManager: InstanceManagerType;
   private logger: DBMLogger;
   private onEvent?: (event: DBMEvent) => void;
   private options: DBMConstructorOptions['options'];
   private onDuckDBShutdown?: () => void;
+  private iFrameManagers: IFrameManager[];
 
   constructor({
     fileManager,
@@ -23,8 +27,9 @@ export class DBMParallel {
     this.logger = logger;
     this.onEvent = onEvent;
     this.options = options;
-    this.instanceManager = instanceManager;
     this.onDuckDBShutdown = onDuckDBShutdown;
+    this.iFrameManagers = [];
+    this.iFrameManagers.push(new IFrameManager('1'));
   }
 
   public async queryWithTables({
@@ -36,9 +41,16 @@ export class DBMParallel {
     tables: TableConfig[];
     options?: QueryOptions;
   }) {
-    const db = await this.instanceManager.getDB();
-    const connection = await db.connect();
-    return connection.query(query);
+    return this.iFrameManagers[0].communication.sendRequest<BrowserRunnerExecQueryMessage>(
+      {
+        type: BROWSER_RUNNER_TYPE.EXEC_QUERY,
+        payload: {
+          query,
+          tables,
+          options,
+        },
+      }
+    );
   }
 
   public async query(query: string) {
--- meerkat-dbm/src/dbm/parallel-dbm/runner-instance-manager.ts
+++ meerkat-dbm/src/dbm/parallel-dbm/runner-instance-manager.ts
@@ -1,28 +1,38 @@
-import { InstanceManagerType } from '../instance-manager';
-import { IFrameManager } from './iframe-manager';
+// import { InstanceManagerType } from '../instance-manager';
+// import { IFrameManager } from './iframe-manager';
+// import { BROWSER_RUNNER_TYPE, BrowserRunnerExecQueryMessage, BrowserRunnerMessage } from '../../window-communication/runner-types';
 
-class Connection {
-  query(query: string) {
-    return query;
-  }
-}
+// class Connection {
+//   query(query: string) {
+//     return query;
+//   }
+// }
 
-class RunnerInstanceManager implements InstanceManagerType {
-  iFrameManagers: IFrameManager[];
-  connection: Connection;
+// class RunnerInstanceManager implements InstanceManagerType {
+//   iFrameManagers: IFrameManager[];
+//   connection: Connection;
 
-  constructor() {
-    this.iFrameManagers = [];
-    this.iFrameManagers.push(new IFrameManager('1'));
-    this.connection = new Connection();
-  }
+//   constructor() {
+//     this.iFrameManagers = [];
+//     this.iFrameManagers.push(new IFrameManager('1'));
+//     this.connection = new Connection();
+//   }
 
-  async getDB() {
-    //Wait for 1s
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+//   private query (query: string) {
+//     this.iFrameManagers[0].communication.sendRequest<BrowserRunnerExecQueryMessage>({
+//         type: BROWSER_RUNNER_TYPE.EXEC_QUERY,
+//         payload: {
+//           query,
+//         }
+//     })
+//   }
 
-    return {
-      connect: async () => this.connection,
-    };
-  }
-}
+//   async getDB() {
+//     //Wait for 1s
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+//     return {
+//       connect: async () => this.connection,
+//     };
+//   }
+// }
