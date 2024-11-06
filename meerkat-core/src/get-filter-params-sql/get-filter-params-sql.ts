663db2cc9c6bd6f17c2c4490ced7bb115b8ba602
zaidjan <zaid.jan@devrev.ai>
2024-11-06 12:39:40 +0530

    formatting fix

--- meerkat-core/src/get-filter-params-sql/get-filter-params-sql.ts
+++ meerkat-core/src/get-filter-params-sql/get-filter-params-sql.ts
@@ -3,37 +3,36 @@ import { getFilterParamsAST } from "../filter-params/filter-params-ast";
 import { FilterType, Query, TableSchema } from "../types/cube-types";
 
 export const getFilterParamsSQL = async ({
+  query,
+  tableSchema,
+  filterType,
+  getQueryOutput
+}: {
+  query: Query;
+  tableSchema: TableSchema;
+  filterType?: FilterType;
+  getQueryOutput: (query: string) => Promise<any>;
+}) => {
+  const filterParamsAST = getFilterParamsAST(
     query,
     tableSchema,
-    filterType,
-    getQueryOutput
-  }: {
-    query: Query;
-    tableSchema: TableSchema;
-    filterType?: FilterType;
-    getQueryOutput: (query: string) => Promise<any>;
-  }) => {
-    const filterParamsAST = getFilterParamsAST(
-      query,
-      tableSchema,
-      filterType
-    );
-    const filterParamsSQL = [];
-    for (const filterParamAST of filterParamsAST) {
-      if (!filterParamAST.ast) {
-        continue;
-      }
-  
-      const queryOutput = await getQueryOutput(astDeserializerQuery(filterParamAST.ast))
-  
-      const sql = deserializeQuery(queryOutput);
-  
-      filterParamsSQL.push({
-        memberKey: filterParamAST.memberKey,
-        sql: sql,
-        matchKey: filterParamAST.matchKey,
-      });
+    filterType
+  );
+  const filterParamsSQL = [];
+  for (const filterParamAST of filterParamsAST) {
+    if (!filterParamAST.ast) {
+      continue;
     }
-    return filterParamsSQL;
-  };
-  
+
+    const queryOutput = await getQueryOutput(astDeserializerQuery(filterParamAST.ast))
+
+    const sql = deserializeQuery(queryOutput);
+
+    filterParamsSQL.push({
+      memberKey: filterParamAST.memberKey,
+      sql: sql,
+      matchKey: filterParamAST.matchKey,
+    });
+  }
+  return filterParamsSQL;
+};
--- meerkat-core/src/get-final-base-sql/get-final-base-sql.ts
+++ meerkat-core/src/get-final-base-sql/get-final-base-sql.ts
@@ -9,25 +9,25 @@ export const getFinalBaseSQL = async ({
   getQueryOutput,
   tableSchema,
 }: {query: Query, tableSchema: TableSchema, getQueryOutput: (query: string) => Promise<any>}) => {
-    /**
-     * Apply transformation to the supplied base query.
-     * This involves updating the filter placeholder with the actual filter values.
-     */
-    const baseFilterParamsSQL = await getFilterParamsSQL({
-      query: query,
-      tableSchema,
-      filterType: 'BASE_FILTER',
-      getQueryOutput,
-    });
-    const baseSQL = applyFilterParamsToBaseSQL(
-      tableSchema.sql,
-      baseFilterParamsSQL
-    );
-    const baseSQLWithFilterProjection = getWrappedBaseQueryWithProjections({
-      baseQuery: baseSQL,
-      tableSchema,
-      query: query,
-    });
-    return baseSQLWithFilterProjection;
-  };
+  /**
+   * Apply transformation to the supplied base query.
+   * This involves updating the filter placeholder with the actual filter values.
+   */
+  const baseFilterParamsSQL = await getFilterParamsSQL({
+    query: query,
+    tableSchema,
+    filterType: 'BASE_FILTER',
+    getQueryOutput,
+  });
+  const baseSQL = applyFilterParamsToBaseSQL(
+    tableSchema.sql,
+    baseFilterParamsSQL
+  );
+  const baseSQLWithFilterProjection = getWrappedBaseQueryWithProjections({
+    baseQuery: baseSQL,
+    tableSchema,
+    query: query,
+  });
+  return baseSQLWithFilterProjection;
+};
   
