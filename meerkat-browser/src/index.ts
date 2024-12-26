cf403b8bdd1e5a065725867de3937ff9e7639296
balasaravanan <115446026+vpbs2@users.noreply.github.com>
2024-12-26 23:34:49 +0530

    update: export

--- meerkat-browser/src/index.ts
+++ meerkat-browser/src/index.ts
@@ -1,4 +1,8 @@
 export * from './browser-cube-to-sql/browser-cube-to-sql';
 export { convertCubeStringToTableSchema };
 import { convertCubeStringToTableSchema } from '@devrev/meerkat-core';
-export { validateDimensionQuery, validateMeasureQuery } from './ast';
+export {
+  parseQueryToAST,
+  validateDimensionQuery,
+  validateMeasureQuery,
+} from './ast';
