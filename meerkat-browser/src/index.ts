1473e757579b5d503b747b6d96d7f91d692eb696
vpbs2 <balasaravananvp02@gmail.com>
2025-06-30 12:46:38 +0530

    fix: package executor

--- meerkat-browser/src/index.ts
+++ meerkat-browser/src/index.ts
@@ -1,10 +1,7 @@
-export * from './browser-cube-to-sql-with-resolution/browser-cube-to-sql-with-resolution';
-export * from './browser-cube-to-sql/browser-cube-to-sql';
-export { convertCubeStringToTableSchema };
-  import { convertCubeStringToTableSchema } from '@devrev/meerkat-core';
 export {
   parseQueryToAST,
   validateDimensionQuery,
-  validateMeasureQuery
+  validateMeasureQuery,
 } from './ast';
-
+export * from './browser-cube-to-sql-with-resolution/browser-cube-to-sql-with-resolution';
+export * from './browser-cube-to-sql/browser-cube-to-sql';
