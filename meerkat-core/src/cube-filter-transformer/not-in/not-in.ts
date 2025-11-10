0a1a1d5658a2b4480bf139cb55a12e6832a13fba
Zaid Jan <98650818+zaidjan-devrev@users.noreply.github.com>
2025-11-10 23:55:11 +0530

    perf(in-filter): Update in filter to use any operator for performance improvements (#165)
    * added any filter
    
    * added not in operator update
    
    * smaller AST
    
    * recd '/Users/zaidjan/Documents/Projects/meerkat'
    
    * updated comments
    
    * updated versions
    
    * added number and string based filtering with string split
    
    * common delimiter
    
    * added combined test cases
    
    * added combined test cases
    
    * elongate the delimiter

--- meerkat-core/src/cube-filter-transformer/not-in/not-in.ts
+++ meerkat-core/src/cube-filter-transformer/not-in/not-in.ts
@@ -1,7 +1,17 @@
 import { Dimension, Measure } from '../../types/cube-types/table';
 import { CubeToParseExpressionTransform } from '../factory';
 
-import { COLUMN_NAME_DELIMITER } from '../../member-formatters/constants';
+import {
+  COLUMN_NAME_DELIMITER,
+  STRING_ARRAY_DELIMITER,
+} from '../../member-formatters/constants';
+import {
+  AggregateHandling,
+  QueryNodeType,
+  ResultModifierType,
+  SubqueryType,
+  TableReferenceType,
+} from '../../types/duckdb-serialization-types';
 import {
   ExpressionClass,
   ExpressionType,
@@ -13,23 +23,24 @@ const notInDuckDbCondition = (
   values: string[],
   memberInfo: Measure | Dimension
 ) => {
-  const sqlTreeValues = values.map((value) => {
-    return {
-      class: ExpressionClass.CONSTANT,
-      type: ExpressionType.VALUE_CONSTANT,
-      alias: '',
-      value: valueBuilder(value, memberInfo),
-    };
-  });
   const columnRef = {
-    class: 'COLUMN_REF',
-    type: 'COLUMN_REF',
+    class: ExpressionClass.COLUMN_REF,
+    type: ExpressionType.COLUMN_REF,
     alias: '',
     column_names: columnName.split(COLUMN_NAME_DELIMITER),
   };
+
   switch (memberInfo.type) {
     case 'number_array':
     case 'string_array': {
+      const sqlTreeValues = values.map((value) => {
+        return {
+          class: ExpressionClass.CONSTANT,
+          type: ExpressionType.VALUE_CONSTANT,
+          alias: '',
+          value: valueBuilder(value, memberInfo),
+        };
+      });
       return {
         class: ExpressionClass.OPERATOR,
         type: ExpressionType.OPERATOR_NOT,
@@ -52,7 +63,7 @@ const notInDuckDbCondition = (
             ],
             filter: null,
             order_bys: {
-              type: 'ORDER_MODIFIER',
+              type: ResultModifierType.ORDER_MODIFIER,
               orders: [],
             },
             distinct: false,
@@ -63,7 +74,186 @@ const notInDuckDbCondition = (
         ],
       };
     }
+    case 'string':
+    case 'number': {
+      /**
+       * Doing the string split optimization here because as the number of nodes in the AST increase,
+       * the time take to parse the AST increases, thereby increasing the time to generate the SQL.
+       */
+      const joinedValues = values.join(STRING_ARRAY_DELIMITER);
+
+      return {
+        class: ExpressionClass.OPERATOR,
+        type: ExpressionType.OPERATOR_NOT,
+        alias: '',
+        children: [
+          {
+            class: ExpressionClass.SUBQUERY,
+            type: ExpressionType.SUBQUERY,
+            alias: '',
+            subquery_type: SubqueryType.ANY,
+            subquery: {
+              node: {
+                type: QueryNodeType.SELECT_NODE,
+                modifiers: [],
+                cte_map: { map: [] },
+                select_list: [
+                  // For numeric types, we need to CAST the string result to the appropriate type
+                  memberInfo.type === 'number'
+                    ? {
+                        class: ExpressionClass.CAST,
+                        type: ExpressionType.OPERATOR_CAST,
+                        alias: '',
+                        child: {
+                          class: ExpressionClass.FUNCTION,
+                          type: ExpressionType.FUNCTION,
+                          alias: '',
+                          function_name: 'unnest',
+                          schema: '',
+                          children: [
+                            {
+                              class: ExpressionClass.FUNCTION,
+                              type: ExpressionType.FUNCTION,
+                              alias: '',
+                              function_name: 'string_split',
+                              schema: '',
+                              children: [
+                                {
+                                  class: ExpressionClass.CONSTANT,
+                                  type: ExpressionType.VALUE_CONSTANT,
+                                  alias: '',
+                                  value: {
+                                    type: { id: 'VARCHAR', type_info: null },
+                                    is_null: false,
+                                    value: joinedValues,
+                                  },
+                                },
+                                {
+                                  class: ExpressionClass.CONSTANT,
+                                  type: ExpressionType.VALUE_CONSTANT,
+                                  alias: '',
+                                  value: {
+                                    type: { id: 'VARCHAR', type_info: null },
+                                    is_null: false,
+                                    value: STRING_ARRAY_DELIMITER,
+                                  },
+                                },
+                              ],
+                              filter: null,
+                              order_bys: {
+                                type: ResultModifierType.ORDER_MODIFIER,
+                                orders: [],
+                              },
+                              distinct: false,
+                              is_operator: false,
+                              export_state: false,
+                              catalog: '',
+                            },
+                          ],
+                          filter: null,
+                          order_bys: {
+                            type: ResultModifierType.ORDER_MODIFIER,
+                            orders: [],
+                          },
+                          distinct: false,
+                          is_operator: false,
+                          export_state: false,
+                          catalog: '',
+                        },
+                        cast_type: {
+                          id: 'DOUBLE',
+                          type_info: null,
+                        },
+                        try_cast: false,
+                      }
+                    : {
+                        class: ExpressionClass.FUNCTION,
+                        type: ExpressionType.FUNCTION,
+                        alias: '',
+                        function_name: 'unnest',
+                        schema: '',
+                        children: [
+                          {
+                            class: ExpressionClass.FUNCTION,
+                            type: ExpressionType.FUNCTION,
+                            alias: '',
+                            function_name: 'string_split',
+                            schema: '',
+                            children: [
+                              {
+                                class: ExpressionClass.CONSTANT,
+                                type: ExpressionType.VALUE_CONSTANT,
+                                alias: '',
+                                value: {
+                                  type: { id: 'VARCHAR', type_info: null },
+                                  is_null: false,
+                                  value: joinedValues,
+                                },
+                              },
+                              {
+                                class: ExpressionClass.CONSTANT,
+                                type: ExpressionType.VALUE_CONSTANT,
+                                alias: '',
+                                value: {
+                                  type: { id: 'VARCHAR', type_info: null },
+                                  is_null: false,
+                                  value: STRING_ARRAY_DELIMITER,
+                                },
+                              },
+                            ],
+                            filter: null,
+                            order_bys: {
+                              type: ResultModifierType.ORDER_MODIFIER,
+                              orders: [],
+                            },
+                            distinct: false,
+                            is_operator: false,
+                            export_state: false,
+                            catalog: '',
+                          },
+                        ],
+                        filter: null,
+                        order_bys: {
+                          type: ResultModifierType.ORDER_MODIFIER,
+                          orders: [],
+                        },
+                        distinct: false,
+                        is_operator: false,
+                        export_state: false,
+                        catalog: '',
+                      },
+                ],
+                from_table: {
+                  type: TableReferenceType.EMPTY,
+                  alias: '',
+                  sample: null,
+                },
+                where_clause: null,
+                group_expressions: [],
+                group_sets: [],
+                aggregate_handling: AggregateHandling.STANDARD_HANDLING,
+                having: null,
+                sample: null,
+                qualify: null,
+              },
+            },
+            child: columnRef,
+            comparison_type: ExpressionType.COMPARE_EQUAL,
+          },
+        ],
+      };
+    }
     default: {
+      // For other types, use the standard COMPARE_NOT_IN approach
+      const sqlTreeValues = values.map((value) => {
+        return {
+          class: ExpressionClass.CONSTANT,
+          type: ExpressionType.VALUE_CONSTANT,
+          alias: '',
+          value: valueBuilder(value, memberInfo),
+        };
+      });
+
       return {
         class: ExpressionClass.OPERATOR,
         type: ExpressionType.COMPARE_NOT_IN,
@@ -76,7 +266,7 @@ const notInDuckDbCondition = (
 
 export const notInTransform: CubeToParseExpressionTransform = (query) => {
   const { member, values, memberInfo } = query;
-  if (!values) {
+  if (!values || values.length === 0) {
     throw new Error('Not in filter must have at least one value');
   }
 
--- meerkat-core/src/member-formatters/constants.ts
+++ meerkat-core/src/member-formatters/constants.ts
@@ -1,2 +1,6 @@
 export const COLUMN_NAME_DELIMITER = '.';
 export const MEERKAT_OUTPUT_DELIMITER = '__';
+
+// Multi-character delimiter using three different uncommon characters
+// to minimize the chance of collision with real data
+export const STRING_ARRAY_DELIMITER = '§‡¶';
