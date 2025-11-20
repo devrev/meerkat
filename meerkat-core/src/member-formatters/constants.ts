e874fb35178fa7ba2c654119025ee2171dc0be8d
zaidjan <zaid.jan@devrev.ai>
2025-11-20 12:50:57 +0530

    reverted in filter

--- meerkat-core/src/member-formatters/constants.ts
+++ meerkat-core/src/member-formatters/constants.ts
@@ -1,5 +1,2 @@
 export const COLUMN_NAME_DELIMITER = '.';
 export const MEERKAT_OUTPUT_DELIMITER = '__';
-// Multi-character delimiter using three different uncommon characters
-// to minimize the chance of collision with real data
-export const STRING_ARRAY_DELIMITER = '§‡¶';
